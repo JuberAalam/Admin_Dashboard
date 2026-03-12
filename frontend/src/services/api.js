19db9962ee95fc7b7cd307229dbeada8b420fb0d
JuberAalam <90861834+JuberAalam@users.noreply.github.com>
2026-03-12 14:37:23 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: import.meta.env.VITE_API_URL ||
+    "https://admin-dashboard-h9ar.onrender.com/api"
 });
 
 // Add token automatically
